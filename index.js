4e085f4b0628d52af9f637209e676522e4a3bee6
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-15 22:32:13 +0200

    Add drag start

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
 //Lets set the eventlisteners
+
+draggables.forEach(draggable=> { 
+  draggable.addEventListener('dragstart', ()=>{
+    //let add a class called dragging to
+    draggable.classList.add('dragging');
+    console.log('drag start');
+  })
+})
