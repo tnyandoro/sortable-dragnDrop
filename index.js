e95a312ef9bdc236c223c799d6ba10a1dd1dfe57
Tendai Nyandoro <tendai1@hotmail.com>
2021-07-15 22:46:31 +0200

    Lop through the elements

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const draggables = document.querySelectorAll('.draggable'); 
+const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
 //Lets set the eventlisteners
 
-draggables.forEach(draggable=> { 
-  draggable.addEventListener('dragstart', ()=>{
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
     //let add a class called dragging to
     draggable.classList.add('dragging');
     console.log('drag start');
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging');
+  })
+});
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault();
+    const draggable = document.querySelector('.dragging');
+    container.appendChild(draggable);
   })
 })
